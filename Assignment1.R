c0f5b0b156512301bdb63a866e46a7e298bdf198
Sourav Ray <me@raysourav.com>
2013-10-06 12:12:08 +0530

    Create Assignment1.R
    initial commit of assignment 1

---
+++ Assignment1.R
@@ -0,0 +1,6 @@
+#Programming Assignment 1 Quiz
+#You need to The zip file containing the data for this assignment from the course home
+
+#read from the data source
+atmdata <- read.csv("hw1_data 2.csv")
+
