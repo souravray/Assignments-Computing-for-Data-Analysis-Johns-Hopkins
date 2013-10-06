1bd830e96ba1edf39c0f62d7ce9861dd720850fe
Sourav Ray <me@raysourav.com>
2013-10-06 12:32:30 +0530

    Update Assignment1.R

--- Assignment1.R
+++ Assignment1.R
@@ -4,3 +4,21 @@
 #read from the data source
 atmdata <- read.csv("hw1_data 2.csv")
 
+#Q1: find column names of the dataset
+names(atmdata)
+#o/p: 
+#   [1] "Ozone"   "Solar.R" "Wind"    "Temp"    "Month"  
+#   [6] "Day"
+
+#Q2: Extract the first 2 rows of the data frame and print them to the console
+firstAtmdata <- atmdata[1:2,]
+firstAtmdata
+#o/p: 
+#       Ozone Solar.R Wind Temp Month Day
+#   1    41     190   7.4   67     5   1
+#   2    36     118   8.0   72     5   2
+
+#Q3: How many observations (i.e. rows) are in this data frame?
+nrow(atmdata)
+#o/p: 
+#   [1] 153 
